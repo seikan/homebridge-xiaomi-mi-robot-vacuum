2acbb26c3fd93c577ee5cf20752867adbe8afc5b
Sei Kan <seikan@live.com>
2017-05-20 16:24:07 +0800

    Changed to use miIO library to communicate with the robot.

--- README.md
+++ README.md
@@ -1,6 +1,88 @@
 # homebridge-xiaomi-mi-robot-vacuum
-[![npm version](https://badge.fury.io/js/homebridge-xiaomi-mi-robot-vacuum.svg)](https://badge.fury.io/js/homebridge-xiaomi-mi-robot-vacuum)
 
-This is XiaoMi Mi robot vacuum plugin for [Homebridge](https://github.com/nfarina/homebridge).
+This is Xiaomi Mi Robot Vacuum plugin for [Homebridge](https://github.com/nfarina/homebridge). Since Apple Homekit is not supporting vacuum cleaner device yet, this plugin will add the robot as **Fan** to your Home app.
 
-![](https://cloud.githubusercontent.com/assets/73107/25977694/d630303c-36ef-11e7-8c6f-12b2266be855.jpg)
+![mi-air-purifier](https://cloud.githubusercontent.com/assets/73107/25977694/d630303c-36ef-11e7-8c6f-12b2266be855.jpg)
+
+### Features
+
+* Switch on / off. When off, it will returning to charging dock automatically.
+
+* Control suction power.
+
+  ​
+
+
+
+
+### Installation
+
+1. Install required packages.
+
+   ```
+   npm install -g homebridge-xiaomi-mi-robot-vacuum miio
+   ```
+
+   ​
+
+2. Remove Mi Robot Vaccum from your Mi Home app.
+
+3. Press "Power" and "Home" button simultaneously and hold for 3 seconds on your Mi Robot Vacuum to reset the Wi-Fi.
+
+   ![mi-robot-reset-wifi](https://cloud.githubusercontent.com/assets/73107/26273343/278c36a2-3d61-11e7-8e08-b5bc25cc407f.png)
+
+4. It will advertised a new Wi-Fi spot similar to`rockrobo-vacumum-v1_xxxx`.
+
+5. Connect your PC to that Wi-Fi network.
+
+6. Open command prompt or terminal. Run following command to discover the device:
+
+   ```
+   miio --discover --sync
+   ```
+
+   **Notes:** This will take about a minute or two.
+
+7. Wait until you get output similar to this:
+
+   ```
+   Device ID: 49466045
+   Model info: Unknown
+   Address: 192.168.8.1
+   Token: 6f7a65786550386b755a6b526666744d via auto-token
+   Support: Unknown
+   ```
+
+8. Record down the value value for `Token` as we need it later.
+
+9. Disconnect from robot Wi-Fi network.
+
+10. Open Mi Home app and add the device as usual.
+
+11. In Mi Home app, get the Robot Vacuum IP address from General Settings > Network Info.
+
+12. Add these values to `config.json`.
+
+    ```
+      "accessories": [
+        {
+          "accessory": "MiRobotVacuum",
+          "name": "Vacuum Cleaner",
+          "ip": "IP_ADDRESS_OF_THE_ROBOT",
+          "token": "TOKEN_DISCOVERED_FROM_STEP_7",
+        }
+      ]
+    ```
+
+    ​
+
+13. Restart Homebridge, and your Mi Robot Vacuum will be added to Home app.
+
+
+
+### License
+
+See the [LICENSE](https://github.com/seikan/homebridge-xiaomi-mi-robot-vacuum/blob/master/LICENSE.md) file for license rights and limitations (MIT).
+
+
+
--- index.js
+++ index.js
@@ -1,76 +1,143 @@
-var dgram = require('dgram');
-var sleep = require('sleep');
-var Service, Characteristic;
+var miio = require('miio');
+var Accessory, Service, Characteristic, UUIDGen;
 
 module.exports = function(homebridge) {
+	Accessory = homebridge.platformAccessory;
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
+	UUIDGen = homebridge.hap.uuid;
 
 	homebridge.registerAccessory('homebridge-xiaomi-mi-robot-vacuum', 'MiRobotVacuum', MiRobotVacuum);
 }
 
 function MiRobotVacuum(log, config) {
-	var platform = this;
 	this.log = log;
+	this.name = config.name || 'Vacuum Cleaner';
+	this.ip = config.ip;
+	this.token = config.token;
+	this.device = null;
 
-	this.name = config.name || 'Mi Robot Vacuum';
-	this.ipAddress = config['ipAddress'];
-	this.start = config['start'];
-	this.stop = config['stop'];
-	this.dock = config['dock'];
+	if(!this.ip)
+		throw new Error('Your must provide IP address of the robot vacuum.');
+
+	if(!this.token)
+		throw new Error('Your must provide token of the robot vacuum.');
+
+	// Vacuum cleaner is not available in Homekit yet, use as Switch
+	this.service = new Service.Fan(this.name);
+
+	this.serviceInfo = new Service.AccessoryInformation();
+
+	this.serviceInfo
+		.setCharacteristic(Characteristic.Manufacturer, 'Xiaomi')
+		.setCharacteristic(Characteristic.Model, 'Robot Vacuum Cleaner')
+		.setCharacteristic(Characteristic.SerialNumber, '526B-0080-4E2D456BF705');
+
+	this.service
+		.getCharacteristic(Characteristic.On)
+		.on('get', this.getOn.bind(this))
+		.on('set', this.setOn.bind(this));
+
+	this.service
+		.getCharacteristic(Characteristic.RotationSpeed)
+		.on('get', this.getRotationSpeed.bind(this))
+		.on('set', this.setRotationSpeed.bind(this));
+
+	this.service
+		.addCharacteristic(Characteristic.BatteryLevel)
+		.on('get', this.getBatteryLevel.bind(this));
+
+	this.discover();
 }
 
 MiRobotVacuum.prototype = {
-	getPowerState: function(callback) {
-		callback(null, false);
+	discover: function(){
+		var accessory = this;
+		var log = this.log;
+
+		log.debug('Discovering Mi Robot Vacuum at "%s"', this.ip);
+
+		this.device = miio.createDevice({
+			address: this.ip,
+			token: this.token,
+			model: 'rockrobo.vacuum.v1'
+		});
+
+		this.device.init()
+			.then(function(){
+				log.debug('Battery Level: ' + accessory.device.battery);
+				log.debug('State: ' + accessory.device.state);
+				log.debug('Fan Power: ' + accessory.device.fanPower);
+			})
+			.catch(function(err){
+				log.debug(err);
+				throw new Error('No able to initialize robot vacuum.');
+			});
 	},
 
-	setPowerState: function(powerOn, callback) {
-		var client = dgram.createSocket('udp4');
-		
-		if(powerOn) {
-			this.log('Starting Mi robot vacuum');
-
-			client.send(new Buffer(this.start, 'hex'), 54321, this.ipAddress, (err) => {
-				this.log('Mi robot vacuum started');
-				client.close();
-			});
-		} else {
-			this.log('Stopping Mi robot vacuum');
-
-			var bufferStop = new Buffer(this.stop, 'hex');
-
-			client.send(new Buffer(this.stop, 'hex'), 54321, this.ipAddress, (err) => {
-				this.log('Mi robot vacuum stopped');
-				sleep.sleep(5);
-
-				client.send(new Buffer(this.dock, 'hex'), 54321, this.ipAddress, (err) => {
-					this.log('Returning to dock');
-					client.close();
-				});
-			});
+	getOn: function(callback) {
+		if(!this.device.state){
+			callback(null, false);
+			return;
 		}
 
+		this.log.debug('State: ' + this.device.state);
+
+		switch(this.device.state){
+			case 'cleaning':
+			case 'returning':
+			case 'paused':
+			case 'spot-cleaning':
+				callback(null, true);
+				break;
+
+			default:
+				callback(null, false);
+		}
+	},
+
+	setOn: function(powerOn, callback) {
+		if(!this.device){
+			callback(new Error('No robot vacuum is discovered.'));
+			return;
+		}
+
+		(powerOn) ? this.device.start() : this.device.charge();
+
 		callback();
+	},
+
+	getRotationSpeed: function(callback){
+		if(!this.device.fanPower){
+			callback(null, 0);
+			return;
+		}
+
+		callback(null, this.device.fanPower);
+	},
+
+	setRotationSpeed: function(speed, callback){
+		if(!this.device){
+			callback();
+			return;
+		}
+		this.device.setFanPower(speed);
+		callback(null, this.device.fanSpeed);
+	},
+
+	getBatteryLevel: function(callback) {
+		if(!this.device.battery){
+			callback(null, 0);
+			return;
+		}
+		callback(null, this.device.battery);
 	},
 
 	identify: function(callback) {
 		callback();
 	},
-	
+
 	getServices: function() {
-		var informationService = new Service.AccessoryInformation();
-
-		informationService
-			.setCharacteristic(Characteristic.Manufacturer, 'Xiao Mi')
-			.setCharacteristic(Characteristic.Model, 'Mi Robot Vacuum');
-
-		switchService = new Service.Switch(this.name);
-		switchService
-			.getCharacteristic(Characteristic.On)
-			.on('get', this.getPowerState.bind(this))
-			.on('set', this.setPowerState.bind(this));
-
-		return [switchService];
+		return [this.serviceInfo, this.service];
 	}
 };
